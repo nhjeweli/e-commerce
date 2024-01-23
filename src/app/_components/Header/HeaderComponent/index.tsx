b77426b9d9d71151b3adc95a728d5419f25890ff
nhjewel <nhjewel75@yahoo.com>
2024-01-24 01:23:38 +0600

    igone typescripts

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -3,13 +3,14 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { HeaderNav } from '../Nav'
-import classes from './index.module.scss'
-import { Gutter } from '../../Gutter'
-
-import { noHeaderFooterUrls } from '../../../constants'
 import { usePathname } from 'next/navigation'
+
 import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constants'
+import { Gutter } from '../../Gutter'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
   const pathname = usePathname()
